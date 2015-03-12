5979e33c4889dae4fd684f5250fd6c13a4d26c4e
Daniel Tyson <danielctyson@gmail.com>
2015-03-12 10:49:29 -0400

    Tests and controllers for creating and signing in users.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def sign_in
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      render json: { token: user.token }
+    else
+      head :unauthorized
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { token: @user.token }
+    else
+      render json: {message: 'failed', status: 500}
+    end
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :address)
+  end
+end
